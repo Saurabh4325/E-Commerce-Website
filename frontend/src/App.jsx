49244a4aa0ce0121074ed456ab161a117425c626
Saurabh Kumar <sbhk4325@gmail.com>
2024-05-06 12:57:17 +0530

    Learing and adding the useContext hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,14 @@ import Footer from './Components/Footer/Footer'
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kids_banner from './Components/Assets/banner_kids.png'
+import all_product from './Components/Assets/all_product'
+import { createContext } from 'react'
+export const userContext=createContext()
 function App() {
-
   return (
 
     <>
+      <userContext.Provider value={all_product}>
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -22,14 +25,14 @@ function App() {
           <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
           <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
           <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />} />
-          <Route path='/products' element={<Product />} />
-          <Route path=':productId' element={<Product />} />
+          <Route path='/product/:id' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
           
         </Routes>
         <Footer />
       </BrowserRouter>
+      </userContext.Provider>
     </>
   )
 }
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import BreadCrum from "../Components/BreadCrum/BreadCrum";
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import { userContext } from '../App';
 
 const Product = () => {
+  const all_product = useContext(userContext);
+  const {id} = useParams();
+  // console.log(id)
+  // console.log(all_product)
+  const product = all_product.find((e) => e.id === Number(id));
+  
   return (
     <div>
-      
+    <BreadCrum product={product} />
+    <ProductDisplay product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
